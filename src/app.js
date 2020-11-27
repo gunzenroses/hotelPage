4d00bd958207194db4688e79ea8f36d0ca89f491
Karina G <coren.gun@gmail.com>
2020-11-27 17:09:37 +0800

    refactor (textFields): apply mixins for textFields

--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ import "./index.pug"
 window.onload = function () {
     require("scripts/data");
     require("scripts/datasetHandler");
+    require("scripts/buttonLike");
     // require("scripts/dropdownRooms");
 };
