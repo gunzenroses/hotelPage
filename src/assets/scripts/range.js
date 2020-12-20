900b3acc5b41e2b4eb5c9f8c8909f334666ad7fc
Karina G <coren.gun@gmail.com>
2020-12-20 14:44:38 +0800

    add: buttons; add: pagination form

--- src/assets/scripts/range.js
+++ src/assets/scripts/range.js
@@ -14,7 +14,7 @@ function setLeftValue(){
     let _this=inputLeft,
         min = parseInt(_this.min),
         max = parseInt(_this.max);
-    _this.value = Math.min(parseInt(_this.value),parseInt(inputRight.value));
+    _this.value = Math.min(parseInt(_this.value),parseInt(inputRight.value)+1);
     let percent = (_this.value - min);
     thumbLeft.style.left = percent+"%";
     range.style.left = percent+"%";
@@ -22,8 +22,8 @@ function setLeftValue(){
 setLeftValue();
 
 function setRightValue(){
-    let _this = inputRight;
-        min = _this.min;
+    let _this = inputRight,
+        min = _this.min,
         max = _this.max;
     
     _this.value = Math.max(parseInt(_this.value),parseInt(inputLeft.value)+ 1);
@@ -33,3 +33,13 @@ function setRightValue(){
     range.style.right= percent+"%";
 }
 setRightValue();
+
+
+/*thumbLeft.addEventListener ('click', function(){
+  inputLeft.style.zIndex = "3";                                                   inputLeft.addEventListener("input", setLeftValue);
+});
+
+thumbRight.addEventListener ("click", ()=>{
+  inputLeft.style.zIndex = "2";
+  inputRight.addEventListener("input", setRightValue);
+});*/
