44635fecadf4e7be74ed4ed777ec8e3ed0f13fcc
Karina G <coren.gun@gmail.com>
2021-01-11 01:35:53 +0800

    add: UIKit.js file to collect paths for all js files for current page

---
+++ src/pages/UIKit/UIKit.js
@@ -0,0 +1,5 @@
+require("../../components/textField/datasetHandler");
+require("../../components/buttonLike/buttonLike");
+require("../../components/range/range");
+require("../../components/pagination/pagination");
+require("../../components/dropdowns/dropdowns");
