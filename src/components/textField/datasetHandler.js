8824b32eaafa7bae024a525621e2fa58535a127d
Karina G <coren.gun@gmail.com>
2021-01-11 15:52:05 +0800

    fix: wrong selector ('input[name]') replaced with ('input[data-rule]')

--- src/components/textField/datasetHandler.js
+++ src/components/textField/datasetHandler.js
@@ -1,22 +1,15 @@
 let maskDate = require('./datasetMaskDate')
-let maskDateRange = require('./datasetMaskDateRange')
+// let maskDateRange = require('./datasetMaskDateRange')
 let dataValitadion = require('./datasetValidation')
 
-let inputToValidate = document.querySelectorAll('input[name]');
+let inputToValidate = document.querySelectorAll('input[data-rule]');
 
 if (inputToValidate) {
     for (let inputField of inputToValidate){
-
-        let name = inputField.name;
-        
-        if (name == "date" || name == "birthday"){
+        let rule = inputField.dataset.rule;
+        if (rule == "birthday"){
             maskDate(inputField);
         }
-
-        if (name == "date-range"){
-            maskDateRange(inputField);
-        }
-
         dataValitadion(inputField);
     }
 }
