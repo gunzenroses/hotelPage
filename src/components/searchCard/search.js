92d0d5f558ea1541a2467cf693b6a3dd048b1b87
Karina G <coren.gun@gmail.com>
2021-01-17 22:11:04 +0800

    feat: styles and html for search card

--- src/components/searchCard/search.js
+++
@@ -1,8 +0,0 @@
-import { Dropdown, renderCalendar } from "../../assets/scripts/myLibrary"
-import { DropdownGuests } from "../../assets/scripts/myLibrary"
-import { guestsData_3 } from "../../assets/scripts/myData"
-
-
-let dateDropdown_3 = new Dropdown("dataRange_3", ".container__dataRange", ".dropdown__calendar")
-let guestsHandler_3 = new DropdownGuests("dropdown_guests_3", ".dropdown__guests", guestsData_3);
-let renderCalendar_3 = new renderCalendar("calendar_3");
