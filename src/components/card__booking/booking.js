6e65b5360cfa5bd57eca6ad672557c5a493b9ded
Karina G <coren.gun@gmail.com>
2021-01-24 01:05:24 +0800

    delete: white space

--- src/components/card__booking/booking.js
+++ src/components/card__booking/booking.js
@@ -2,5 +2,4 @@ import { renderCalendar, DropdownGuests } from "../../assets/scripts/myLibrary"
 import { guestsData_2 } from "../../assets/scripts/myData"
 
 let calendar_4 = new renderCalendar("calendar_dataRange_4");
-
 let guestsHandler_4 = new DropdownGuests("dropdown_guests_4", ".dropdown__guests", guestsData_2);
