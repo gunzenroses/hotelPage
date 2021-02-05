0d3ba47a261a06894ac03e09d7d55334545bd136
Karina G <coren.gun@gmail.com>
2021-02-05 23:24:41 +0800

    feat: conditional request for UIKit code chunks

--- src/app.js
+++ src/app.js
@@ -2,5 +2,9 @@ import "./index.scss"
 import "./index.pug"
 
 window.onload = function () {
+    require("./components/textField/datasetHandler");
+    require("./components/checkboxExpandable/checkboxExpandable");
+    require("./components/carousel/carousel");
+    
     require("./pages/UIKit/UIKit");
 };
--- src/pages/UIKit/UIKit.js
+++ src/pages/UIKit/UIKit.js
@@ -1,12 +1,9 @@
-require("../../components/textField/datasetHandler");
-require("../../components/buttonLike/buttonLike");
-require("../../components/range/range");
-require("../../components/pagination/pagination");
-require("../../components/dropdowns/dropdowns");
-require("../../components/checkboxExpandable/checkboxExpandable");
-require("../../components/calendar/calendar");
-
-
-require("../../components/card__search/search");
-require("../../components/card__booking/booking");
-require("../../components/carousel/carousel");
+if (document.getElementById("dataRange_2")){
+    require("../../components/buttonLike/buttonLike");
+    require("../../components/range/range");
+    require("../../components/pagination/pagination");
+    require("../../components/dropdowns/dropdowns");
+    require("../../components/calendar/calendar");
+    require("../../components/card__search/search");
+    require("../../components/card__booking/booking");
+}
