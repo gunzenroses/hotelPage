d08cf7848d59d2f1bb99825bb111e7888b2b45c8
Karina G <coren.gun@gmail.com>
2021-03-02 15:23:32 +0800

    refactor: files for starting page

--- src/app.js
+++ src/app.js
@@ -6,5 +6,13 @@ window.onload = function () {
     require("./components/checkboxExpandable/checkboxExpandable");
     require("./components/carousel/carousel");
     
-    require("./pages/UIKit/UIKit");
+    // if (document.getElementById("dataRange_2")){
+    //     require("./components/buttonLike/buttonLike");
+    //     require("./components/range/range");
+    //     require("./components/pagination/pagination");
+    //     require("./components/dropdowns/dropdowns");
+    //     require("./components/calendar/calendar");
+    //     require("./components/card__search/search");
+    //     require("./components/card__booking/booking");
+    // }
 };
