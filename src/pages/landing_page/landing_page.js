eb6943a7dfe9902bfee3ea962ee446efbbf46ab8
Karina G <coren.gun@gmail.com>
2021-03-05 15:05:22 +0800

    add [landing_page.js]: card_search dependancies

--- src/pages/landing_page/landing_page.js
+++ src/pages/landing_page/landing_page.js
@@ -2,4 +2,5 @@ import "./landing_page.scss"
 
 window.onload = function () {
     require("../../components/checkboxExpandable/checkboxExpandable");
+    require("../../components/card__search/search");
 }
