b8e23d403b28404e60601f5f5366887acc87cae5
Karina G <coren.gun@gmail.com>
2021-03-05 10:22:20 +0800

    feat: different eventListeners for dropdownWatched and dropdownInits lists

--- src/components/checkboxExpandable/checkboxExpandable.js
+++ src/components/checkboxExpandable/checkboxExpandable.js
@@ -1,65 +1,52 @@
+// form a list of class "dropdown__watch";
+let dropdownWatchs = document.querySelectorAll(".dropdown__watch");
+
+//for all dropdownWatchs add eventlistener
+for (let dropdownWatch of dropdownWatchs){
+    dropdownWatch.addEventListener("click", ()=>{
+        let dropdownWatchedInits = dropdownWatch.querySelectorAll(".dropdown__init");
+
+        let dropdownWatchedParents = [];
+            for (let l=0;l<dropdownWatchedInits.length;l++){
+                dropdownWatchedParents.push(dropdownWatchedInits[l].parentElement);
+            }
+
+        let dropdownWatchedExpands = [];
+            for (let m=0;m<dropdownWatchedParents.length;m++){
+                dropdownWatchedExpands.push(dropdownWatchedParents[m].querySelector(".dropdown__content"));
+            }
+        
+        // if you click on dropdownExpanded
+        if (event.target.closest(".dropdown__show")){
+
+            // and want to expand its item
+            if (event.target.closest(".dropdown__init")){
+
+                let innerParent = event.target.closest(".dropdown__init").parentElement;
+                let innerExpand = innerParent.querySelector(".dropdown__content");
+                //then toggle
+                innerExpand.classList.toggle("dropdown__show");
+            }
+        } else {
+            for (let n=0;n<dropdownWatchedExpands.length;n++){
+                if (event.target.closest(".dropdown__init") != dropdownWatchedInits[n]){
+                    dropdownWatchedExpands[n].classList.remove("dropdown__show");
+                } else {
+                    dropdownWatchedExpands[n].classList.toggle("dropdown__show");
+                }
+            }
+        }
+    })
+}
 
 // form a list of class "dropdown__init"
 let dropdownInits = document.querySelectorAll(".dropdown__init");
-
-
-// init functionality for every element of the list
 for (let dropdownInit of dropdownInits){
-    let dropdownParent = dropdownInit.parentElement;
-    let dropdownExpanded = dropdownParent.querySelector(".dropdown__content");
-
-    if (dropdownParent.closest(".dropdown__watch")){
-        let dropdownWatch = dropdownParent.closest(".dropdown__watch");
-
-        dropdownWatch.addEventListener("click", ()=>{
-            if (event.target != dropdownInit){
-                dropdownExpanded.classList.remove("dropdown__show");
-            } else {
-                dropdownExpanded.classList.toggle("dropdown__show");
-            }
-        })
-    } else {
-
-    dropdownInit.addEventListener("click", ()=>{
-        dropdownExpanded.classList.toggle("dropdown__show");
-    });
-    
+    if (!dropdownInit.closest(".dropdown__watch")){
+        let dropdownParent = dropdownInit.parentElement;
+        let dropdownExpanded = dropdownParent.querySelector(".dropdown__content");
+        dropdownInit.addEventListener("click", ()=>{
+            dropdownExpanded.classList.toggle("dropdown__show");
+        });
     }
 }
-
-// the following code was replaced
-// class Dropdown {
-//     constructor(containerId, buttonSelector, contentSelector){
-//         this.container = document.getElementById(containerId)
-//         this.containerClass = this.container.classList.value
-//         this.dropdownButton = this.container.querySelector(buttonSelector);
-//         this.dropdownExpanded = this.container.querySelector(contentSelector)
-//         this.init()
-//     }
-
-//     init(){
-//         this.createChildren();
-//         this.enableHandlers();
-//         this.enableEventListeners();
-//         return this;
-//     }
-
-//     createChildren(){
-//         this.containerClassExpanded = "." + this.containerClass;
-//         return this;
-//     }
-
-//     enableHandlers(){
-//         this.toggleHandler = this.toggleExpanded.bind(this);
-//     }
-
-//     enableEventListeners(){
-//         this.dropdownButton.addEventListener("click", this.toggleHandler);
-//         return this;
-//     }
-
-//     toggleExpanded(){
-//         this.dropdownExpanded.classList.toggle("dropdown__show");
-//         return this;
-//     }
-// }
