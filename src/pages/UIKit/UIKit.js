19ba99c912f3cb5927106d8acbeea0582fb6b1d3
Karina G <coren.gun@gmail.com>
2021-03-06 14:33:53 +0800

    restructure: require neat files for cards, delete condition for required files

--- src/pages/UIKit/UIKit.js
+++ src/pages/UIKit/UIKit.js
@@ -1,18 +1,26 @@
 import "./UIkit.scss"
 import "./UIKit.pug"
+import { renderCalendar } from "../../assets/scripts/myLibrary"
+
 
 window.onload = function () {
     require("../../components/textField/datasetHandler");
     require("../../components/checkboxExpandable/checkboxExpandable");
     require("../../components/carousel/carousel");
 
-    if (document.getElementById("dataRange_2")){
-        require("../../components/buttonLike/buttonLike");
-        require("../../components/range/range");
-        require("../../components/pagination/pagination");
-        require("../../components/dropdowns/dropdowns");
-        require("../../components/calendar/calendar");
-        require("../../components/card__search/search");
-        require("../../components/card__booking/booking");
-    }
+
+    require("../../components/buttonLike/buttonLike");
+    require("../../components/range/range");
+    require("../../components/pagination/pagination");
+    require("../../components/dropdowns/dropdowns");
+
+    //for formElements
+    let calendar_1 = new renderCalendar("calendar_1");
+    let calendar_2 = new renderCalendar("calendar_2");
+
+    // for cards
+    require("../../components/card__search/search");
+    require("../../components/card__booking/booking");
+    require("../../components/calendar/calendar");
+    
 }
