6423c72783244965c1b6452881956d3b6cce1afd
Karina G <coren.gun@gmail.com>
2021-03-07 14:27:00 +0800

    restucture: separate code for dropdownGuests and dropdownRooms objects

--- src/components/dropdowns/dropdowns.js
+++
@@ -1,9 +0,0 @@
-import { DropdownGuests, DropdownRooms } from "../../assets/scripts/myLibrary"
-import { guestsData_1, guestsData_booking, roomsData_1, roomsData_2 } from "../../assets/scripts/myData"
-
-let roomsHandler = new DropdownRooms ("dropdown_rooms_1", ".dropdown__rooms", roomsData_1);
-let roomsHandler_2 = new DropdownRooms ("dropdown_rooms_2", ".dropdown__rooms", roomsData_2);
-
-let guestsHandler_0 = new DropdownGuests("dropdown_guests_0", ".dropdown__guests", );
-let guestsHandler_1 = new DropdownGuests("dropdown_guests_1", ".dropdown__guests", guestsData_1);
-let guestsHandler_2 = new DropdownGuests("dropdown_guests_2", ".dropdown__guests", guestsData_booking);
