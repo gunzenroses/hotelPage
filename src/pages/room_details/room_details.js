68b695c7939d6a3b89c7f8beb9332191309b5735
Karina G <coren.gun@gmail.com>
2021-04-09 11:50:26 +0800

    init: room_details page

---
+++ src/pages/room_details/room_details.js
@@ -0,0 +1,6 @@
+import "./room_details.scss"
+
+window.onload = function () {
+    require("../../components/checkboxExpandable/checkboxExpandable");
+    require("../../components/card__search/search");
+}
