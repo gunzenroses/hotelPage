aee89971dc96dd089fed20c0c0253ff5d3000ed0
Karina G <coren.gun@gmail.com>
2021-04-13 12:09:50 +0800

    add: dependancies and objects of likeButton class

--- src/pages/room_details/room_details.js
+++ src/pages/room_details/room_details.js
@@ -1,6 +1,10 @@
+
 import "./room_details.scss"
+import { likeButtons } from "../../components/buttonLike/buttonLike"
 
 window.onload = function () {
     require("../../components/checkboxExpandable/checkboxExpandable");
     require("../../components/card__booking/booking");
+    let likeButton_1 = new likeButtons("likes_author_1", 12);
+    let likeButton_2 = new likeButtons("likes_author_2", 2);
 }
