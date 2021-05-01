6b8c5ac6eb53ead3b6c570474d30eab71c7a7c4b
Karina G <coren.gun@gmail.com>
2021-05-01 18:07:25 +0800

    feat: update dependancy with replaced range slider

--- src/pages/searchRoom/searchRoom.js
+++ src/pages/searchRoom/searchRoom.js
@@ -1,5 +1,5 @@
 import "./searchRoom.scss"
-import { Range } from "../../components/range/range"
+//import { Range } from "../../components/range/range"
 import { DropdownRooms } from "../../components/dropdownRooms/dropdownRooms"
 import { roomsData_1 } from "../../assets/scripts/myData"
 
@@ -12,11 +12,12 @@ import { ButtonPagination } from "../../components/pagination/pagination"
 window.onload = function(){
     require("../../components/textField/datasetHandler");
     require("../../components/checkboxExpandable/checkboxExpandable");
+    require("../../components/range/range");
 
     // require("../../components/card_search/card_search");
     let guests_searchRoom = new DropdownGuests("dropdown_guests_search", guestsData_searchRoom);
 
-    let range_search = new Range("range_search");
+    //let range_search = new Range("range_search");
     let dropdownRooms_search = new DropdownRooms("dropdown_rooms_1", roomsData_1);
 
     let pagination_search = new ButtonPagination(paginationData_1, "pagination_search")
