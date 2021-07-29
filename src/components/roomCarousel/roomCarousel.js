1196ac35875ffd78befd16b753b076fa15ee53e9
Karina G <coren.gun@gmail.com>
2021-07-29 19:33:42 +0800

    fix: change naming style: from camelCase -> hyphen-case

--- src/components/roomCarousel/roomCarousel.js
+++ src/components/roomCarousel/roomCarousel.js
@@ -1,55 +1,55 @@
 // choose all containers for carousel
 
-let carousels = document.querySelectorAll(".roomCarousel");
+let carousels = document.querySelectorAll(".room-carousel");
 
 // do the following code for every carousel
 for (let carousel of carousels){
     
-    let carouselInputs = carousel.getElementsByClassName("roomCarousel__radio")[0];
-    let carouselPictures = carousel.querySelector(".roomCarousel__pictures");
+    let carouselInputs = carousel.getElementsByClassName("room-carousel__radio")[0];
+    let carouselPictures = carousel.querySelector(".room-carousel__pictures");
     let leftValue = carouselPictures.style.left;
-    carouselInputs.querySelectorAll(".roomCarousel__radio_real")[0].checked = true;
+    carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
     carouselPictures.style.left = 0;
     
-    if (carousel.querySelector(".roomCarousel__btn_prev")){
-        let carouselPrev = carousel.querySelector(".roomCarousel__btn_prev");
+    if (carousel.querySelector(".room-carousel__btn_prev")){
+        let carouselPrev = carousel.querySelector(".room-carousel__btn_prev");
         carouselPrev.addEventListener("click", () => {
             leftValue += 100;
             switch(leftValue){
                 case 0:  
-                        carouselInputs.querySelectorAll(".roomCarousel__radio_real")[0].checked = true;
+                        carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
                         carouselPictures.style.left = leftValue + "%";
                         break;
-                case -100: carouselInputs.querySelectorAll(".roomCarousel__radio_real")[1].checked = true;
+                case -100: carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
                             carouselPictures.style.left = leftValue + "%";
                             break;
-                case -200: carouselInputs.querySelectorAll(".roomCarousel__radio_real")[2].checked = true;
+                case -200: carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
                             carouselPictures.style.left = leftValue + "%";
                             break;
                 case 100: leftValue = -300;
-                        carouselInputs.querySelectorAll(".roomCarousel__radio_real")[3].checked = true;
+                        carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
                         carouselPictures.style.left = leftValue + "%";            
                         break;
             }
         })
     };
 
-    if (carousel.querySelector(".roomCarousel__btn_next")){
-        let carouselNext = carousel.querySelector(".roomCarousel__btn_next");
+    if (carousel.querySelector(".room-carousel__btn_next")){
+        let carouselNext = carousel.querySelector(".room-carousel__btn_next");
         carouselNext.addEventListener("click", () => {
             leftValue -= 100;
             switch(leftValue){
                 case -400:  leftValue = 0;
-                            carouselInputs.querySelectorAll(".roomCarousel__radio_real")[0].checked = true;
+                            carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
                             carouselPictures.style.left = leftValue + "%";
                             break;
-                case -100: carouselInputs.querySelectorAll(".roomCarousel__radio_real")[1].checked = true;
+                case -100: carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
                             carouselPictures.style.left = leftValue + "%";
                             break;
-                case -200: carouselInputs.querySelectorAll(".roomCarousel__radio_real")[2].checked = true;
+                case -200: carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
                             carouselPictures.style.left = leftValue + "%";
                             break;
-                case -300: carouselInputs.querySelectorAll(".roomCarousel__radio_real")[3].checked = true;
+                case -300: carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
                             carouselPictures.style.left = leftValue + "%";            
                             break;
             }
