f4fdea2fbe3774edbc726ad128dda84a4f1155a0
Karina G <coren.gun@gmail.com>
2021-08-02 00:56:35 +0800

    refactor: refine code

--- src/components/room-carousel/roomCarousel.js
+++ src/components/room-carousel/roomCarousel.js
@@ -1,113 +1,95 @@
-// choose all containers for carousel
+export default function makeCarousels() {
+  let carousels = document.querySelectorAll(".room-carousel");
+  for (let carousel of carousels) {
+    new Carousel(carousel);
+  }
+}
 
-let carousels = document.querySelectorAll(".room-carousel");
+class Carousel {
+  constructor(carousel) {
+    this.carousel = carousel;
+    this.carouselInputs = this.carousel.getElementsByClassName("room-carousel__radio")[0];
+    this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
+    this.carouselPictures = this.carousel.querySelector(".room-carousel__pictures");
+    this.carouselPictures.style.left = 0;
+    this.leftValue = this.carouselPictures.style.left;
+    this.init();
+  }
 
-// do the following code for every carousel
-for (let carousel of carousels) {
+  init() {
+    this.creatChildren();
+    this.setupHandlers();
+    this.enable();
+  }
 
-  let carouselInputs = carousel.getElementsByClassName("room-carousel__radio")[0];
-  let carouselPictures = carousel.querySelector(".room-carousel__pictures");
-  let leftValue = carouselPictures.style.left;
-  carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
-  carouselPictures.style.left = 0;
+  creatChildren() {
+    this.carouselPrev = this.carousel.querySelector(".room-carousel__btn_prev")
+      ? this.carousel.querySelector(".room-carousel__btn_prev")
+      : null;
+    console.log(this.carouselPrev)
+    this.carouselNext = (this.carousel.querySelector(".room-carousel__btn_next"))
+      ? this.carousel.querySelector(".room-carousel__btn_next")
+      : null;
+  }
 
-  if (carousel.querySelector(".room-carousel__btn_prev")) {
-    let carouselPrev = carousel.querySelector(".room-carousel__btn_prev");
-    carouselPrev.addEventListener("click", () => {
-      leftValue += 100;
-      switch (leftValue) {
-        case 0:
-          carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case -100: carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case -200: carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case 100: leftValue = -300;
-          carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-      }
-    })
-  };
+  setupHandlers() {
+    this.prevImageHandler = this.prevImage.bind(this);
+    this.nextImgHandler = this.nextImage.bind(this);
+    this.switchImageHandler = this.switchImage.bind(this);
+  }
 
-  if (carousel.querySelector(".room-carousel__btn_next")) {
-    let carouselNext = carousel.querySelector(".room-carousel__btn_next");
-    carouselNext.addEventListener("click", () => {
-      leftValue -= 100;
-      switch (leftValue) {
-        case -400: leftValue = 0;
-          carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case -100: carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case -200: carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-        case -300: carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
-          carouselPictures.style.left = leftValue + "%";
-          break;
-      }
-    })
-  };
+  enable() {
+    if (this.carouselPrev) this.carouselPrev.addEventListener("click", () => { this.prevImageHandler; })
+    if (this.carouselNext) this.carouselNext.addEventListener("click", () => { this.nextImgHandler; })
+    this.carouselInputs.addEventListener("click", () => { this.switchImageHandler; })
+  }
 
-  carouselInputs.addEventListener("click", event => {
-    switch (event.target.value) {
+  prevImage() {
+    leftValue += 100;
+    switch (leftValue) {
+      case 0:
+        this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case -100: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case -200: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case 100: leftValue = -300;
+        this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+    }
+  }
+
+  nextImage() {
+    leftValue -= 100;
+    switch (leftValue) {
+      case -400: leftValue = 0;
+        this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case -100: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case -200: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+      case -300: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
+        this.carouselPictures.style.left = leftValue + "%";
+        break;
+    }
+  }
+
+  switchImage(e) {
+    switch (e.target.value) {
       case "picture_1": leftValue = 0; break;
       case "picture_2": leftValue = -100; break;
       case "picture_3": leftValue = -200; break;
       case "picture_4": leftValue = -300; break;
     }
-    carouselPictures.style.left = leftValue + "%";
-  });
+    this.carouselPictures.style.left = leftValue + "%";
+  }
 }
-
-
-
-
-// class Carousel {
-//     constructor(selector){
-//         this.carouselContainer = selector;
-//         this.init()
-//     }
-
-//     init(){
-//         this.createChildren();
-//         this.enableHandlers();
-//         this.enableEventListeners();
-//         return this;
-//     }
-
-//     createChildren(){
-//         this.carouselInputs = this.carouselContainer.getElementsByClassName("carousel__radio")[0];
-//         this.carouselPictures = this.carouselContainer.querySelector(".carousel__pictures");
-//         return this;
-//     }
-
-//     enableHandlers(){
-//         this.choosePictureHandler = this.choosePicture.bind(this);
-//         return this;
-//     }
-
-//     enableEventListeners(){
-//         this.carouselInputs.addEventlistener("click", this.choosePictureHandler);
-//         return this;
-//     }
-
-//     choosePicture(){
-//         let leftValue;
-//         switch(event.target.value){
-//             case "picture_1": leftValue = 0; break;
-//             case "picture_2": leftValue = -100; break;
-//             case "picture_3": leftValue = -200; break;
-//             case "picture_4": leftValue = -300; break;
-//         }
-//         this.carouselPictures.style.left = leftValue+"%";
-//         return this;
-//     }
-// }
