087997c12612af33303f82dd2f8b30b6612204d8
Karina G <coren.gun@gmail.com>
2021-08-03 16:52:25 +0800

    fix: leftValue: parseInt(this.carouselPictures.style.left)

--- src/components/room-carousel/roomCarousel.js
+++ src/components/room-carousel/roomCarousel.js
@@ -12,7 +12,7 @@ class Carousel {
     this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
     this.carouselPictures = this.carousel.querySelector(".room-carousel__pictures");
     this.carouselPictures.style.left = 0;
-    this.leftValue = this.carouselPictures.style.left;
+    this.leftValue = parseInt(this.carouselPictures.style.left);
     this.init();
   }
 
@@ -26,7 +26,6 @@ class Carousel {
     this.carouselPrev = this.carousel.querySelector(".room-carousel__btn_prev")
       ? this.carousel.querySelector(".room-carousel__btn_prev")
       : null;
-    console.log(this.carouselPrev)
     this.carouselNext = (this.carousel.querySelector(".room-carousel__btn_next"))
       ? this.carousel.querySelector(".room-carousel__btn_next")
       : null;
@@ -39,57 +38,57 @@ class Carousel {
   }
 
   enable() {
-    if (this.carouselPrev) this.carouselPrev.addEventListener("click", () => { this.prevImageHandler; })
-    if (this.carouselNext) this.carouselNext.addEventListener("click", () => { this.nextImgHandler; })
+    if (this.carouselPrev) this.carouselPrev.addEventListener("click", this.prevImageHandler)
+    if (this.carouselNext) this.carouselNext.addEventListener("click", this.nextImgHandler)
     this.carouselInputs.addEventListener("click", () => { this.switchImageHandler; })
   }
 
   prevImage() {
-    leftValue += 100;
-    switch (leftValue) {
+    this.leftValue += 100;
+    switch (this.leftValue) {
       case 0:
         this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
       case -100: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
       case -200: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
-      case 100: leftValue = -300;
+      case 100: this.leftValue = -300;
         this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
     }
   }
 
   nextImage() {
-    leftValue -= 100;
-    switch (leftValue) {
-      case -400: leftValue = 0;
+    this.leftValue -= 100;
+    switch (this.leftValue) {
+      case -400: this.leftValue = 0;
         this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[0].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
       case -100: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[1].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
       case -200: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[2].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
       case -300: this.carouselInputs.querySelectorAll(".room-carousel__radio_real")[3].checked = true;
-        this.carouselPictures.style.left = leftValue + "%";
+        this.carouselPictures.style.left = this.leftValue + "%";
         break;
     }
   }
 
   switchImage(e) {
     switch (e.target.value) {
-      case "picture_1": leftValue = 0; break;
-      case "picture_2": leftValue = -100; break;
-      case "picture_3": leftValue = -200; break;
-      case "picture_4": leftValue = -300; break;
+      case "picture_1": this.leftValue = 0; break;
+      case "picture_2": this.leftValue = -100; break;
+      case "picture_3": this.leftValue = -200; break;
+      case "picture_4": this.leftValue = -300; break;
     }
-    this.carouselPictures.style.left = leftValue + "%";
+    this.carouselPictures.style.left = this.leftValue + "%";
   }
 }
