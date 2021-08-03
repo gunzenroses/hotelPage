6160fd3496e22e81950576b30382502f1d2c1633
Karina G <coren.gun@gmail.com>
2021-08-03 21:35:11 +0800

    delete: redundant whitespaces and commented code

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -3,7 +3,6 @@ import "./room-details.scss"
 import ButtonLike from "Main/components/button-like/buttonLike"
 import ExpandItems from "Main/components/checkbox-expandable/expandItems"
 
-
 window.onload = function () {
     require("Main/components/card-booking/cardBooking");
     let likeButton_1 = new ButtonLike("likes_author_1", 12);
