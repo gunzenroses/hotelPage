0d40f7a7617c859d20f13522c07b4a04fc45046f
Karina G <coren.gun@gmail.com>
2021-08-13 13:15:45 +0800

    delete: import of custom-made mask for inputs

--- src/components/text-field/datasetHandler.js
+++ src/components/text-field/datasetHandler.js
@@ -1,14 +1,9 @@
-import maskDate from 'Main/components/text-field/datasetMaskDate'
 import dataValidation from 'Main/components/text-field/datasetValidation'
 
 const inputToValidate = document.querySelectorAll('input[data-rule]');
 
 if (inputToValidate) {
   for (let inputField of inputToValidate) {
-    let rule = inputField.dataset.rule;
-    if (rule == "birthday") {
-      maskDate(inputField);
-    }
     dataValidation(inputField);
   }
 }
