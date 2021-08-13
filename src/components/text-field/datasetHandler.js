e91ccc8a2b2e144340ef499f3359e07e9ef11974
Karina G <coren.gun@gmail.com>
2021-08-13 11:38:16 +0800

    delete: commented code

--- src/components/text-field/datasetHandler.js
+++ src/components/text-field/datasetHandler.js
@@ -12,30 +12,3 @@ if (inputToValidate) {
     dataValidation(inputField);
   }
 }
-
-
-
-
-
-
-/*
-    To validate the Date
-*/
-// function checkDate(year, month, day){
-//     let date = new Date(year, month-1, day);
-//     return (date.getFullYear == year
-//         && date.getMonth == month
-//         && date.getDate == day);
-// }
-
-/*
-
-*/
-// function validateForm(selector, rules){
-//     new window.JustValitade(selector, {
-//         rules: rules,
-//         submitHandler: function(form, values, ajax){
-//             console.log(form);
-//         }
-//     }
-// }
