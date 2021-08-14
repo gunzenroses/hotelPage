6a8258e065c0a98a9f0c360b3ea1055830b1d0c4
Karina G <coren.gun@gmail.com>
2021-08-14 21:40:34 +0800

    replace: 'datasetHandler.js' -> 'validate-registration' and 'validate-signin'

--- src/pages/UIKit/UIKit.js
+++ src/pages/UIKit/UIKit.js
@@ -13,31 +13,33 @@ import "./UIkit.scss"
 import "./UIKit.pug"
 
 window.onload = function () {
-    require("Components/range/range");
-    require("Components/text-field/inputMask");
-    require("Components/text-field/datasetHandler");
-    require("Components/card-search/cardSearch");
-    require("Components/card-booking/cardBooking");
-    makeCarousels();
+  require("Components/footer/validate-footer-form");
+  require("Components/card-registration/validate-registration");
+  require("Components/card-singin/validate-signin");
+  require("Components/card-search/cardSearch");
+  require("Components/card-booking/cardBooking");
+  require("Components/range/range");
+  require("Scripts/inputMasks");
+  makeCarousels();
 
-    let dropdownWatches = Array.from(document.querySelectorAll(".js-dropdown__watch"));
-    dropdownWatches.forEach(item => {
-      new ExpandItems(item);
-    })
-    
-    let likeButton_1 = new ButtonLike("buttonLike_1", 2);
-    let likeButton_2 = new ButtonLike("buttonLike_2", 12);
-    let likeButtons_3 = new ButtonLike("likes_author_1", 12);
-    let pagination_formElements = new ButtonPagination(paginationData_1, "pagination_bar_1");
-    
-    let calendar_1 = new Calendar("calendar_1");
-    let calendar_2 = new Calendar("calendar_2");
-    let calendar_simple = new Calendar("calendar_simple")
+  let dropdownWatches = Array.from(document.querySelectorAll(".js-dropdown__watch"));
+  dropdownWatches.forEach(item => {
+    new ExpandItems(item);
+  })
 
-    let guestsHandler_0 = new DropdownGuests("dropdown_guests_0", );
-    let guestsHandler_1 = new DropdownGuests("dropdown_guests_1", guestsData_1);
-    let guestsHandler_2 = new DropdownGuests("dropdown_guests_2", guestsData_booking);
-    
-    let roomsHandler = new DropdownRooms ("dropdown_rooms_1", roomsData_1);
-    let roomsHandler_2 = new DropdownRooms ("dropdown_rooms_2", roomsData_2);
+  let likeButton_1 = new ButtonLike("buttonLike_1", 2);
+  let likeButton_2 = new ButtonLike("buttonLike_2", 12);
+  let likeButtons_3 = new ButtonLike("likes_author_1", 12);
+  let pagination_formElements = new ButtonPagination(paginationData_1, "pagination_bar_1");
+
+  let calendar_1 = new Calendar("calendar_1");
+  let calendar_2 = new Calendar("calendar_2");
+  let calendar_simple = new Calendar("calendar_simple")
+
+  let guestsHandler_0 = new DropdownGuests("dropdown_guests_0",);
+  let guestsHandler_1 = new DropdownGuests("dropdown_guests_1", guestsData_1);
+  let guestsHandler_2 = new DropdownGuests("dropdown_guests_2", guestsData_booking);
+
+  let roomsHandler = new DropdownRooms("dropdown_rooms_1", roomsData_1);
+  let roomsHandler_2 = new DropdownRooms("dropdown_rooms_2", roomsData_2);
 }
