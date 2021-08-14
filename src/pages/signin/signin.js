b58f425d7e9b9e102b55db159cebbdca36fc7fa5
Karina G <coren.gun@gmail.com>
2021-08-14 21:48:04 +0800

    replace: 'datasetHandler' -> 'validate-footer-form' + 'validate-signin'

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -3,7 +3,8 @@ import ExpandItems from "Components/checkbox-expandable/expandItems";
 import "./signin.scss";
 
 window.onload = function(){
-  require("Components/text-field/datasetHandler");
-  require("Components/text-field/inputMask");
+  require("Components/footer/validate-footer-form");
+  require("Components/card-singin/validate-signin");
+  require("Scripts/inputMasks");
   let activateExpand = new ExpandItems();
 }
