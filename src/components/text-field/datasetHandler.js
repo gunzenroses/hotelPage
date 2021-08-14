f84ed4aa08c6790c77ff8dad5ab5bb956522ae65
Karina G <coren.gun@gmail.com>
2021-08-14 19:48:15 +0800

    delete: custom validation

--- src/components/text-field/datasetHandler.js
+++
@@ -1,9 +0,0 @@
-import dataValidation from 'Main/components/text-field/datasetValidation'
-
-const inputToValidate = document.querySelectorAll('input[data-rule]');
-
-if (inputToValidate) {
-  for (let inputField of inputToValidate) {
-    dataValidation(inputField);
-  }
-}
