f9c285831a7242743d861147361cccb3e70a7b20
Karina G <coren.gun@gmail.com>
2021-08-14 20:02:08 +0800

    add: validation rules and messages to forms: - registration; - sigin; - footer.

---
+++ src/components/card-registration/validate-registration.js
@@ -0,0 +1,63 @@
+require("jquery-validation");
+require("Scripts/validationMethods.js");
+
+$('.card-registration').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 2,
+    },
+    surname: {
+      required: true,
+      minlength: 2,
+    },
+    birthday: {
+      required: true,
+    },
+    email: {
+      required: true,
+      email: true,
+      minlength: 7,
+    },
+    password: {
+      required: true,
+      pwcheck: true,
+      lowerCase: true,
+      upperCase: true,
+      hasDigit: true,
+      minlength: 8,
+    }
+  },
+  messages: {
+    name: {
+      required: "Введите Имя",
+      minlength: "Имя должно быть длинее 2 символов",
+    },
+    surname: {
+      required: "Введите Фамилию",
+      minlength: "Фамилия должна быть длинее 2 символов",
+    },
+    birthday: {
+      required: "Введите дату рождения",
+    },
+    email: {
+      required: "Введите email",
+      email: "Некорректный email",
+      minlength: "Поле должно быть длинее 7 символов",
+    },
+    password: {
+      required: "Введите пароль",
+      pwcheck: "Содержит недопустимые значения",
+      lowerCase: "Пароль должен содержать хотя бы 1 строчную букву",
+      upperCase: "Пароль должен содержать хотя бы 1 заглавную букву",
+      hasDigit: "Пароль должен содержать хотя бы 1 цифру",
+      minlength: "Пароль должен быть длинее 8 символов",
+    }
+  },
+  submitHandler: function(form) {                
+    form.submit();
+  },
+  errorPlacement: function(error, element) {                 
+    element.parent().after(error);
+  }
+})
---
+++ src/components/card-singin/validate-signin.js
@@ -0,0 +1,41 @@
+require("jquery-validation");
+require("Scripts/validationMethods.js");
+
+$(".card-signin").validate({
+  rules: {
+    email: {
+      required: true,
+      email: true,
+      minlength: 7,
+    },
+    password: {
+      required: true,
+      pwcheck: true,
+      lowerCase: true,
+      upperCase: true,
+      hasDigit: true,
+      minlength: 8,
+    }
+  },
+  messages: {
+    email: {
+      required: "Введите email",
+      email: "Некорректный email",
+      minlength: "Поле должно быть длинее 7 символов",
+    },
+    password: {
+      required: "Введите пароль",
+      pwcheck: "Содержит недопустимые значения",
+      lowerCase: "Пароль должен содержать хотя бы 1 строчную букву",
+      upperCase: "Пароль должен содержать хотя бы 1 заглавную букву",
+      hasDigit: "Пароль должен содержать хотя бы 1 цифру",
+      minlength: "Пароль должен быть длинее 8 символов",
+    }
+  },
+  submitHandler: function(form) {                
+    form.submit();
+  },
+  errorPlacement: function(error, element) {                 
+    element.parent().after(error);
+  }
+})
