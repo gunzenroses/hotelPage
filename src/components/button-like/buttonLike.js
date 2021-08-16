3f04f4f4c49bee5811df052042c0f8f2425501e7
Karina G <coren.gun@gmail.com>
2021-08-17 05:38:10 +0800

    rename: shorten names of js-classes of button-like_heart & co -> b-l__h

--- src/components/button-like/buttonLike.js
+++ src/components/button-like/buttonLike.js
@@ -13,8 +13,8 @@ export default class ButtonLike {
   }
 
   createChildren() {
-    this.number = this.likeButton.querySelector('.js-button-like__number');
-    this.heart = this.likeButton.querySelector('.js-button-like__heart');
+    this.number = this.likeButton.querySelector('.js-b-l__n');
+    this.heart = this.likeButton.querySelector('.js-b-l__h');
   }
 
   enableHandlers() {
