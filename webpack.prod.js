3ce96db6dddc70ef5267f18d8a438da311e8ee74
Karina G <coren.gun@gmail.com>
2021-08-16 16:07:04 +0800

    add: soureMap for sass-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,63 +6,68 @@ const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: "production",
-    output: {
-        path: path.resolve(__dirname, "./dist"),
-        publicPath: "https://gunzenroses.github.io/hotelPage/",
-        filename: "assets/js/[name].[hash:7].bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,   // for parallel loading of CSS/JS resources later on
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            sourceMap: true,
-                            minimize: true,
-                            colormin: false,
-                        },
-                    },
-                ],
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "https://gunzenroses.github.io/hotelPage/",
+    filename: "assets/js/[name].[hash:7].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,   // for parallel loading of CSS/JS resources later on
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+              minimize: true,
+              colormin: false,
             },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, // creates style nodes from JS strings
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            minimize: true,
-                            sourceMap: true,
-                            colormin: false,
-                        },
-                    }, 
-                    "postcss-loader", // translates CSS into CommonJS
-                    "sass-loader", // compiles Sass to CSS
-                ],
-            },
-        ]
-    },
-
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                parallel: true,
-                sourceMap: true,
-            }),
+          },
         ],
-    },
-
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "assets/css/[name].[hash:7].bundle.css",
-            chunkFilename: "[id].css",
-        }),
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              minimize: true,
+              sourceMap: true,
+              colormin: false,
+            },
+          },
+            "postcss-loader", // translates CSS into CommonJS
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+            options: {
+              sourceMap: true,
+            },
+          }
+        ],
+      },
     ]
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].[hash:7].bundle.css",
+      chunkFilename: "[id].css",
+    }),
+  ]
 })
