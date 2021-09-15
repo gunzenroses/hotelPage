b25d7fa5310bd66ecaa277f6a83b845551d85a69
gunzenroses <coren.gun@gmail.com>
2021-09-15 19:45:16 +0800

    refactor: rename 'js-dropdown__watch' -> 'js-dropdown__detection'

--- src/components/checkbox-expandable/makeExpandable.js
+++ src/components/checkbox-expandable/makeExpandable.js
@@ -1,7 +1,7 @@
 import ExpandItems from './ExpandItems';
 
 export default function makeExpandable() {
-  const dropdownWatches = Array.from(document.querySelectorAll('.js-dropdown__watch'));
+  const dropdownWatches = Array.from(document.querySelectorAll('.js-dropdown__detection'));
   dropdownWatches.forEach((item) => {
     new ExpandItems(item);
   });
