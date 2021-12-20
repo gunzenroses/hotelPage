26ff6fd37d3c4a3fd144798ccf8da914bfb80966
gunzenroses <coren.gun@gmail.com>
2021-12-21 03:29:23 +0800

    feat: match js-file name to class name

--- src/components/card-booking/card-booking.js
+++ src/components/card-booking/card-booking.js
@@ -1,6 +1,6 @@
 import Calendar from 'Main/components/calendar/Calendar';
 import applyClass from 'Main/assets/scripts/applyClass';
-import DropdownGuests from 'Components/dropdown/_type_guests/dropdown_type_guests';
+import DropdownGuests from 'Main/components/dropdown/_type_guests/DropdownGuests';
 import { guestsDataBooking } from 'Main/assets/scripts/my-data';
 
 applyClass('.js-calendar', Calendar);
--- src/components/card-search/card-search.js
+++ src/components/card-search/card-search.js
@@ -2,7 +2,7 @@
 import Calendar from 'Main/components/calendar/Calendar';
 import applyClass from 'Main/assets/scripts/applyClass';
 import { guestsDataSearch } from 'Main/assets/scripts/my-data';
-import DropdownGuests from 'Components/dropdown/_type_guests/dropdown_type_guests';
+import DropdownGuests from 'Main/components/dropdown/_type_guests/DropdownGuests';
 
 applyClass('.js-calendar', Calendar);
 applyClass([{ id: 'dropdown_guests_search', data: guestsDataSearch }], DropdownGuests);
