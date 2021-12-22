8e4a88447123522735707e3d78d62842456c1b22
gunzenroses <coren.gun@gmail.com>
2021-12-22 22:38:05 +0800

    delete: redundant whitespaces

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -1,6 +1,5 @@
 import applyClass from 'Scripts/applyClass';
 import 'Scripts/inputmasks';
-
 import Expand from 'Components/expand/Expand';
 import 'Components/card-singin/card-signin';
 import 'Components/footer/footer';
