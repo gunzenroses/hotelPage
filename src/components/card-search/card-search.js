9da711f171f233aeda435dc75c711dc01f0e879e
gunzenroses <coren.gun@gmail.com>
2022-01-07 14:27:39 +0800

    #2: upd paths for 'scripts' folder

--- src/components/card-search/card-search.js
+++ src/components/card-search/card-search.js
@@ -1,8 +1,8 @@
 // this exemplar will work for all pages
-import Calendar from 'Main/components/calendar/Calendar';
-import applyClass from 'Main/assets/scripts/applyClass';
-import { guestsDataSearch } from 'Main/assets/scripts/my-data';
-import DropdownGuests from 'Main/components/dropdown/_type_guests/DropdownGuests';
+import applyClass from 'Scripts/applyClass';
+import { guestsDataSearch } from 'Scripts/my-data';
+import Calendar from 'Components/calendar/Calendar';
+import DropdownGuests from 'Components/dropdown/_type_guests/DropdownGuests';
 
 applyClass('.js-calendar', Calendar);
 applyClass([{ id: 'dropdown_guests_search', data: guestsDataSearch }], DropdownGuests);
