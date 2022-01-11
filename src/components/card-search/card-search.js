78b6b137190f9043eba4a36b96b3de3e745646f9
gunzenroses <coren.gun@gmail.com>
2022-01-11 23:26:49 +0800

    delete: comment

--- src/components/card-search/card-search.js
+++ src/components/card-search/card-search.js
@@ -1,4 +1,3 @@
-// this exemplar will work for all pages
 import applyClass from 'Scripts/applyClass';
 import Calendar from 'Main/components/calendar/Calendar';
 import DropdownGuests from 'Components/dropdown/_type_guests/DropdownGuests';
