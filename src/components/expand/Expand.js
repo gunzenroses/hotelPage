b006fc68e906aa916f2642052b07ca6259480d8f
gunzenroses <coren.gun@gmail.com>
2022-01-11 19:58:39 +0800

    #4: use PascalCase for class-naming

---
+++ src/components/expand/Expand.js
@@ -0,0 +1,69 @@
+class Expand {
+  constructor(dropdownWatch) {
+    this.dropdownWatch = dropdownWatch;
+    this.init();
+  }
+
+  init() {
+    this.createChildren();
+    this.setupHandlers();
+    this.enable();
+  }
+
+  createChildren() {
+    this.dropdownWatchedParents = [];
+    this.dropdownWatchedExpands = [];
+    this.dropdownWatchedInits = Array.from(this.dropdownWatch.querySelectorAll('.js-expand__init'));
+    this.dropdownWatchedInits.forEach((item) => {
+      this.dropdownWatchedParents.push(item.parentElement);
+    });
+
+    this.dropdownWatchedParents.forEach((item) => {
+      this.dropdownWatchedExpands.push(item.querySelector('.js-expand__content'));
+    });
+  }
+
+  setupHandlers() {
+    this.dropdownClickHandler = this.dropdownClick.bind(this);
+  }
+
+  enable() {
+    this.dropdownWatch.addEventListener('click', this.dropdownClickHandler);
+  }
+
+  dropdownClick(event) {
+    const hasInnerExpand = event.target.closest('.js-expand__show');
+    const hasExpand = event.target.closest('.js-expand__init');
+    const hasParent = event.target.parentElement;
+    const noExpand = !hasInnerExpand && !hasExpand;
+    const el = event.target;
+
+    if (hasInnerExpand && hasExpand) this.expandElement(el);
+    if (!hasInnerExpand && hasExpand) this.expandInner(el);
+    if (noExpand && hasParent) this.hideElements();
+  }
+
+  expandElement(el) {
+    const innerParent = el.closest('.js-expand__init').parentElement;
+    const innerExpand = innerParent.querySelector('.js-expand__content');
+    innerExpand.classList.toggle('js-expand__show');
+  }
+
+  expandInner(el) {
+    this.dropdownWatchedExpands.forEach((item, index) => {
+      if (el.closest('.js-expand__init') !== this.dropdownWatchedInits[index]) {
+        item.classList.remove('js-expand__show');
+      } else {
+        item.classList.toggle('js-expand__show');
+      }
+    });
+  }
+
+  hideElements() {
+    this.dropdownWatchedExpands.forEach((item) => {
+      item.classList.remove('js-expand__show');
+    });
+  }
+}
+
+export default Expand;
