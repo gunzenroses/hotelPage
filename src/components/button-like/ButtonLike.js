c1f082b19dabb6bde26af605bbe5fb4cf2be8aa4
gunzenroses <coren.gun@gmail.com>
2022-01-25 21:41:08 +0800

    fix: ButtonLike class

--- src/components/button-like/ButtonLike.js
+++ src/components/button-like/ButtonLike.js
@@ -19,24 +19,17 @@ class ButtonLike {
   }
 
   enableEventListeners() {
-    this.buttonLike.addEventListener("click", this.buttonLikeIncrease);
+    this.buttonLike.addEventListener("click", this.render);
   }
-
+  
   @boundMethod
-  buttonLikeIncrease() {
-    this.data += 1;
-    this.render();
-  }
-
   render() {
-    this.number.textContent = this.data;
-    if (parseInt(this.number.textContent, 10) > 9) this.makePopularClass();
-  }
-
-  makePopularClass() {
-    this.buttonLike.classList.add("button-like_popular");
-    this.heart.classList.add("button-like__heart_popular");
-    this.number.classList.add("button-like__number_popular");
+    this.number.textContent = this.heart.classList.contains(
+      "button-like__heart_popular"
+    ) ? this.data++ : this.data--;
+    this.buttonLike.classList.toggle("button-like_popular");
+    this.heart.classList.toggle("button-like__heart_popular");
+    this.number.classList.toggle("button-like__number_popular");
   }
 }
 
