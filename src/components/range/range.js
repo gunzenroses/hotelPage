67ce8f5b3938439ffd6a94ea3c10a4542c3bc239
gunzenroses <coren.gun@gmail.com>
2022-02-03 18:26:17 +0800

    #7: initiate ion-rangeslider as Range class in 'libs/' folder

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -1,22 +1,5 @@
-import 'ion-rangeslider';
+import Range from 'Libs/range/Range';
 
-function changeValue(data) {
-  const toPretty = data.to_pretty ? data.to_pretty : '10 000';
-  const fromPretty = data.from_pretty ? data.from_pretty : '5 000';
-  const rangeValue = $(data.input).siblings('.js-range__value');
-  rangeValue.val(`${ fromPretty }₽ - ${ toPretty }₽`);
-}
-
-$('.js-range__slider').ionRangeSlider({
-  type: 'double',
-  min: 0,
-  max: 15000,
-  from: 5000,
-  to: 10000,
-  hide_min_max: true,
-  hide_from_to: true,
-  prettify_enabled: true,
-  step: 100,
-  onStart: changeValue,
-  onChange: changeValue
+document.addEventListener('DOMContentLoaded', () => {
+  new Range();
 });
---
+++ src/libs/range/Range.js
@@ -0,0 +1,32 @@
+import 'ion-rangeslider';
+
+class Range {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    $('.js-range__slider').ionRangeSlider({
+      type: 'double',
+      min: 0,
+      max: 15000,
+      from: 5000,
+      to: 10000,
+      hide_min_max: true,
+      hide_from_to: true,
+      prettify_enabled: true,
+      step: 100,
+      onStart: this.changeValue,
+      onChange: this.changeValue,
+    });
+  }
+
+  changeValue(data) {
+    const toPretty = data.to_pretty ? data.to_pretty : '10 000';
+    const fromPretty = data.from_pretty ? data.from_pretty : '5 000';
+    const rangeValue = $(data.input).siblings('.js-range__value');
+    rangeValue.val(`${fromPretty}₽ - ${toPretty}₽`);
+  }
+}
+
+export default Range;
